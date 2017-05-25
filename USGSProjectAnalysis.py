7421cd60312820704c4604e419cd36fff698c4cd
Margaret Knoblock <mrk7217@gmail.com>
2017-05-24 17:22:49 -0700

    postSoftware method completed and adds software with name, description, operating systems and version number. main method now works if usgsData is already downloaded. if not need to run read url first

--- USGSProjectAnalysis.py
+++ USGSProjectAnalysis.py
@@ -7,10 +7,13 @@ from urllib.request import urlopen
 import requests
 import json
 
-def main(): #not working yet
+def main():
     #readURL(url, fileName)
     usgsData = getInfo('usgsData')
-    url = 'http://usgs.ontosoft.org/repository/login'
+    loginUrl = 'http://usgs.ontosoft.org/repository/login'
+    ID = authenticateUser(loginUrl, 'mknoblock','testPassword')
+    for software in usgsData:
+        postSoftware(software, ID)
 
 def authenticateUser(url, username, password):
     '''Authenticates user and password on usgs portal of ontosoft
@@ -24,16 +27,27 @@ def authenticateUser(url, username, password):
 
 def postSoftware(softwareInfo, sessionID):
     '''Adds software to USGS Ontosoft portal with softwareInfo given in the format
-    [name, description, os, version].
+    [name, description, [os], version]. If software name already exists in portal, will
+    not post anything but request will go through.
     Input: list softwareInfo, string sessionID
     Output: none'''
-    #still need to add values information for description, os and version
+    osVals = []
     url = 'http://usgs.ontosoft.org/repository/software'
     headers = {'content-type': 'application/json','X-Ontosoft-Session':sessionID}
     nameInfo = {"@type":"TextEntity", "type":"http://ontosoft.org/software#TextEntity"}
+    descInfo = {"@type":"TextEntity", "type":"http://ontosoft.org/software#TextEntity"}
+    versionInfo = {"@type":"EnumerationEntity", "id":"http://usgs.ontosoft.org/repository/software/SoftwareVersion", "type":"http://ontosoft.org/software#SoftwareVersion"}
+    for os in softwareInfo[2]:
+        osVals += [{"@type":"EnumerationEntity", "type":"http://ontosoft.org/software#OperatingSystem", "value": os}]
+    print(osVals)
     values = {}
     nameInfo['value'] = softwareInfo[0]
+    descInfo['value'] = softwareInfo[1]
+    versionInfo['value'] = softwareInfo[3]
     values["http://ontosoft.org/software#hasName"] = [nameInfo]
+    values["http://ontosoft.org/software#hasShortDescription"] = [descInfo]
+    values["http://ontosoft.org/software#supportsOperatingSystem"] = osVals
+    values["http://ontosoft.org/software#hasSoftwareVersion"] =[versionInfo]
     data = {"@type":"Software","value":values,"label":softwareInfo[0]}
     response = requests.post(url, data = json.dumps(data), headers = headers)
     return response
@@ -53,7 +67,7 @@ def getInfo(fileName):
     ''' Reads and formats information from text file according to style
     of USGS website
     Input: String file name
-    Output: ? '''
+    Output: list of software informations where each software has a list of information '''
     file = open(fileName, 'r', encoding='utf-8')
     atList = False
     allSoftwares = []
@@ -70,24 +84,23 @@ def getInfo(fileName):
 def formatLine(line):
     ''' Takes in a string of information about a software and creates a list with
     the information organized in the following format:
-    [name, description, os, version]
+    [name, description, [os], version]
     Input: string line
     Output: list '''
     software = ['','','','']
     version = False
-    os = ''
+    formattedOS = []
     versionNum = ''
     separated = line.split('\u2028') #splits information by identifier for new line
     if 'Version' in separated[0]: #if there is a version number
         version = True
         versionInd = separated[0].index('Version')
 
-    #if there is an operating system it will be within the first 20 characters and
-    #have a ( at the beginning, title will end before operating system
     if '(' in separated[0]: #if there is an os
         osInd = separated[0].index('(') #starting index for os
         title = separated[0][3:osInd] #starts at 5 to eliminate spacing and bullet point
         os = separated[0][osInd+1:line.index(')')]
+        formattedOS = formatOS(os)
     elif version:
         title = separated[0][3:versionInd]
     elif ',' in separated[0]: #if there is a date
@@ -101,8 +114,43 @@ def formatLine(line):
     elif version:
         versionNum = separated[0][versionInd:]
 
-    return [title, separated[1], os, versionNum]
-    #this needs to be some sort of dictionary with specified data names as keys 
+    return [title, separated[1].strip('\n'), formattedOS, versionNum]
+
+def formatOS(osString):
+    ''' This function takes in an osString which may have multiple OSes separated
+    by '/' and puts them into the list. Also changes OS abbreviations as specified
+    on USGS website and puts them into full form
+    Input: string of OS abbreviations
+    Output: list of OS full names'''
+    if osString == '':
+        numOS = 0
+    else: 
+        numOS = 1 + osString.count('/')
+    os = []
+    while numOS > 0:
+        if numOS == 1:
+            osAbbrev = osString
+        else:
+            ind = osString.index('/')
+            osAbbrev = osString[0:ind]
+            osString = osString[ind+1:]
+        osName = fullName(osAbbrev)
+        os += [osName]
+        numOS -= 1
+    return os
+            
+def fullName(osAbbrev):
+    ''' Given an OS abbreviation, returns the full OS name. OS names and abbreviations
+    given by https://water.usgs.gov/software/lists/alphabetical under 'Abbreviations for OSs section'
+    Input: string osAbbrev
+    Output: string osName'''
+    osName = osAbbrev
+    OSs = {'DOS':'IBM-compatible PC', 'DG':'Data General AViiON DG/UX', 'Mac':'Macintosh',\
+           'SGI': 'Silicon Graphics Indigo', 'Sun': 'Sun SPARCstation Solaris', 'Win': 'Microsoft Windows'}
+    if osAbbrev in OSs:
+        osName = OSs[osAbbrev]
+    return osName
+
 
 
 
